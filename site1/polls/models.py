6584e5918b07c130a8366edb6a2a755acd701145
Admia1 <sampadpars@gmail.com>
2019-05-18 23:03:58 +0430

    user module updated

--- site1/polls/models.py
+++ site1/polls/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Puser(models.Model):
+class PollUser(models.Model):
     user = models.OneToOneField(User ,on_delete=models.CASCADE)
-    can_presure = models.BooleanField(default=0)
-    more_detail_1 = models.CharField(max_length=200, default="")
-    more_detail_2 = models.CharField(max_length=200, default="")
 
+    first_name = models.CharField(max_length=100, default="FIRST_NAME")
+    last_name = models.CharField(max_length=100, default="LAST_NAME")
+    phone_number  = models.CharField(max_length=20, default="0917")
+    student_number = models.CharField(max_length=20, default="1234")
+    entry_year = models.IntegerField()
+
+    can_presure = models.BooleanField(default=0)
+    
 class Section(models.Model):
     work_date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
-    related_users = models.ManyToManyField(Puser)
+    related_users = models.ManyToManyField(PollUser)
