b429596f8e3391cafe6b4d3be7f081aa2a7bdbb8
Admia1 <sampadpars@gmail.com>
2019-05-20 17:58:19 +0430

    need to make it persian detail page remaining count err may be there creating sections

--- site2/polls/admin.py
+++ site2/polls/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import PollUser,Section, EventDay
+from .models import PollUser,Section, EventDay, USR
 # Register your models here.
 
 admin.site.register(PollUser)
 admin.site.register(Section)
 admin.site.register(EventDay)
+admin.site.register(USR)
