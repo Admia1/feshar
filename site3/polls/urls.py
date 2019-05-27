908a660ac3c95e4cb823f1fa98fbff81469cc5f1
Admia1 <sampadpars@gmail.com>
2019-05-27 10:16:54 +0430

    commenting take from urls.py

--- site3/polls/urls.py
+++ site3/polls/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     url('login/', views.login_view, name='login'),
     url('logout.', views.logout_view, name='logout'),
     url('home/', views.home_view, name='home'),
-    url('take/', views.take_view ,name='take'),
+    #url('take/', views.take_view ,name='take'),
     path('delete/<int:usr_pk>/', views.delete_view ,name='delete'),
     path('info/<int:polluser_pk>/', views.info_user_view, name='userinfo'),
-     url('info/', views.info_view, name='info'),
+    url('info/', views.info_view, name='info'),
     url('', views.register_view, name='base'),
 ]
