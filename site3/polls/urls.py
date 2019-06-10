2cc7970ad5f63c15943c2c523cfaedf1b2a4a9cf
Admia1 <sampadpars@gmail.com>
2019-06-10 21:33:35 +0430

    fixing future bugs changing all urls to path

--- site3/polls/urls.py
+++ site3/polls/urls.py
@@ -4,17 +4,17 @@ from django.urls import path
 
 app_name = 'polls' # So we can use it like: {% url 'mymodule:user_register' %} on our template.
 urlpatterns = [
-    url('register/', views.register_view, name='register'),
-    url('login/', views.login_view, name='login'),
-    url('logout.', views.logout_view, name='logout'),
-    url('home/', views.home_view, name='home'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout.', views.logout_view, name='logout'),
+    path('home/', views.home_view, name='home'),
     path('users/', views.users_view, name='users'),
-    url('ngp/', views.ngp_student_view, name='ngp'),
-    url('gp/', views.gp_student_view, name='gp'),
-    url('cant/', views.cant_view, name='cant'),
-    url('get_national_id/', views.get_national_id_view, name='get_national_id'),
-    url('table_shift/', views.table_shift_view, name='table_shift'),
-    url('table_user/', views.table_user_view , name='table_user'),
+    path('ngp/', views.ngp_student_view, name='ngp'),
+    path('gp/', views.gp_student_view, name='gp'),
+    path('cant/', views.cant_view, name='cant'),
+    path('get_national_id/', views.get_national_id_view, name='get_national_id'),
+    path('table_shift/', views.table_shift_view, name='table_shift'),
+    path('table_user/', views.table_user_view , name='table_user'),
 
 
     #url('take/', views.take_view ,name='take'),
@@ -23,6 +23,6 @@ urlpatterns = [
     path('section/<int:section_pk>/', views.section_view, name='section'),
 
     path('chang_usr_present/<int:usr_pk>/<int:new_state>/', views.change_present_view, name='chang_usr_present'),
-    url('sections/', views.sections_view, name='sections'),
-    url('', views.register_view, name='base'),
+    path('sections/', views.sections_view, name='sections'),
+    path('', views.register_view, name='base'),
 ]
