fd3d2088bb3c833bc148121194d471b5e6ead4ef
Admia1 <sampadpars@gmail.com>
2019-07-21 16:24:56 +0430

    removing site2

--- site2/polls/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import PollUser,Section, EventDay, USR
-# Register your models here.
-
-admin.site.register(PollUser)
-admin.site.register(Section)
-admin.site.register(EventDay)
-admin.site.register(USR)
